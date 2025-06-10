71023e077d71b43419d95fbf7dd8f286fb75c5b0
Muhammad Kafaby <114895955+Muhammadkafaby@users.noreply.github.com>
2025-06-10 14:11:59 +0000

    deploy vercel

--- api/index.py
+++ api/index.py
@@ -2,6 +2,6 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from app import app as vercel_app
+from app import app
 
-# Vercel will use 'vercel_app' as the entry point for the serverless function
+# Vercel expects 'app' as the entry point for Python serverless functions
